fa59364faf7e35d2bba4b46aab2381f50c67e10a
Reuben Ayres <ra@sent.com>
2018-10-27 08:25:56 -0500

    api.  weekend HW detail

---
+++ controllers/fruitAPIController.js
@@ -0,0 +1,154 @@
+const express = require('express');
+// Next we set up the Router
+const router = express.Router();
+// require Our Model - Remember Model is
+// a representation of our data
+// The model should capitalized
+const Fruits = require('../models/fruits');
+// index route should show all the fruits
+
+router.get('/', (req, res) => {
+  // finding every fruit without a search parameter
+  Fruits.find({}, (err, allFruits) => {
+    if(err){
+      const errorResponse = {
+        status: 404, // this is probably not the ideal code here
+        success: false, // booleans are nice because they let FEDs write
+                        // logic easily
+        message: "Error with database query",
+        error: err
+      }
+      res.json(errorResponse);
+    } else {
+      const response = {
+        status: 200,
+        success: true,
+        message: allFruits.length + " fruits found",
+        data: allFruits
+      }
+      res.json(response);
+    };
+  });
+});
+
+// create route
+router.post('/', (req, res) => {
+  // contents of the form will be in req.body
+
+
+  console.log(req.body, 'this is req.body, should be form info');
+  if(req.body.readyToEat === 'on'){
+    req.body.readyToEat = true;
+  } else {
+    req.body.readyToEat = false;
+  }
+  // adding the contents of the form to the model
+  Fruits.create(req.body, (err, createdFruit)=> {
+    if(err){
+      const errorResponse = {
+        success: false,
+        status: 422,
+        message: "could not add to database",
+        error: err
+      }
+      res.json(errorResponse);
+    } else {
+      const response = {
+        success: true,
+        status: 201,
+        message: 'successfully added ' + createdFruit.name,
+        data: createdFruit
+      }
+      res.json(response);
+    }
+  });
+});
+
+
+// Show Route
+router.get('/:id', (req, res) => {
+  Fruits.findById(req.params.id, (err, foundFruit) => {
+    if(err) {
+      const errorResponse = {
+        status: 204, // this is probably not the ideal code here
+        success: false, // booleans are nice because they let FEDs write
+                        // logic easily
+        message: "Error with database query",
+        error: err
+      } 
+      res.json(errorResponse);     
+    } else {
+      const response = {
+        success: true,
+        status: 200,
+        message: "found " + foundFruit.name,
+        data: foundFruit
+      }
+      res.json(response);
+    }
+  })
+
+});
+
+router.put('/:id', (req, res) => {
+  console.log(' am I hitting the put route') // Check to see if im hitting im route
+  // If Im not hitting the route, there is probably something with the action of form
+
+  // If it is hitting the route, I want to see
+  console.log(req.body, 'Why: IT tells what is coming from the form')
+
+  if(req.body.readyToEat === 'on'){ // if checked then req.body.readyToEat = 'on'
+    req.body.readyToEat = true;
+  } else {
+    req.body.readyToEat = false;
+  }
+  // req.body is the updated form info
+  //new true, says return to me the updated object, by default it is false
+  // things that are default you don't have to specify
+
+  // first argument, is the document you are looking for
+  // second argument, is the content you are updating with
+  Fruits.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedFruit) => {
+    if(err){
+      res.send(err);
+    } else {
+        // Check to see if it is updating correctly
+        console.log(updatedFruit, ' CHeck our model')
+        res.redirect('/fruits');
+    }
+  })
+
+});
+
+
+// Delete route
+router.delete('/:id', (req, res) => {
+
+  // Delete a specific fruit
+  console.log(req.params.id, ' this is params in delete')
+
+  Fruits.findByIdAndRemove(req.params.id, (err, deletedFruit) => {
+    if(err){
+      const errorResponse = {
+        success: false,
+        status: 418, // teapot
+        message: "could not delete",
+        error: err
+      }
+      res.json(errorResponse)
+    } else {
+      const response = {
+        success: true,
+        status: 200,
+        message: "successfully deleted fruit " + req.params.id,
+        deletedFruit: deletedFruit // you may or may not need to do this
+
+      }
+      res.json(response);
+    }
+  });
+})
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,48 @@
+
+// THE MOST IMPORTANT THING THIS WEEKEND IS REACT TUTORIAL AND HELLO WORLD
+
+// 1. READ REACT HELLO WORLD 
+
+// 2. READ REACT HELLO WORLD AGAIN ALL OF IT
+
+// 3. DO THE REACT TUTORIAL -- if you prefer to "learn by doing" you could start with this
+
+// 4. READ REACT HELLO WORLD A THIRD TIME
+
+// ----------
+
+// 5. finish update API route
+
+// 6. make it take POST and PUT requests where the  
+// body is JSON instead of x-www-form-urlencoded
+// (reminder: in postman choose "Raw" and then JSON from the menu that appears)
+
+// 7. read about CORS and try to set it up in this API -- 
+// as basic as possible so that ANY client can talk to your API
+    // you may need to do #3 to get #2 to work
+    // there is an npm module called "cors" -- try to follow instructions on npm.org
+    // page for that module  
+
+// 8. Hungry for more re: APIs?  
+// See if you can set up some routes that do pagination!
+// a URL for this endpoint might be 
+  // GET '/api/fruits/:pageNumber' 
+  // GET '/api/fruits/:resultsPerPage/:pageNumber/'
+// ----------
+
+// 9. Debug your project 2 app/deployment!  
+// Don't add features, just take what you got and 
+  // • get rid of all the weird errors and unexpected behavior
+  // • spend a little time making it look nice and professional visually
+
+
+
 const express        = require('express');
 const app            = express();
 const bodyParser     = require('body-parser');
 const methodOverride = require('method-override');
+// REQUIRE CORS HERE
+
 
 // require our db
 require('./db/db');
@@ -12,13 +53,17 @@ require('./db/db');
 // the app.use sets up what middleware you are using
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(methodOverride('_method'));
+// USE CORS HERE
+
 
 // Require the controller after the middleware
 const fruitController = require('./controllers/fruitController');
+const fruitAPIController = require('./controllers/fruitAPIController');
 
 // This means every route in the fruitController
 // now starts with /fruits
 app.use('/fruits', fruitController);
+app.use('/api/fruits', fruitAPIController);
 
 
 
